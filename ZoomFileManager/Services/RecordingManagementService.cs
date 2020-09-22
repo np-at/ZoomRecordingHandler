b3c4e4c620f41562d981cb518dabbd7f56527a96
Noah Praskins <noahpraskins@gmail.com>
2020-09-22 16:35:51 -0700

    fix

--- ZoomFileManager/Services/RecordingManagementService.cs
+++ ZoomFileManager/Services/RecordingManagementService.cs
@@ -50,9 +50,9 @@ namespace ZoomFileManager.Services
             foreach (var item in webhookEvent.Payload.Object.RecordingFiles)
             {
                 var req = new HttpRequestMessage(HttpMethod.Get, item.DownloadUrl);
-                if (!string.IsNullOrWhiteSpace(webhookEvent.DownloadToken))
+                if (!string.IsNullOrWhiteSpace(webhookEvent.DownloadToken ?? webhookEvent.Payload.DownloadToken))
                     req.Headers.Authorization =
-                        AuthenticationHeaderValue.Parse($"Bearer ${webhookEvent.DownloadToken}");
+                        AuthenticationHeaderValue.Parse($"Bearer ${webhookEvent.DownloadToken ?? webhookEvent.Payload.DownloadToken}");
                 req.Headers.Add("Accept", "application/json");
                 requests.Add((req, fileNameTransformationFunc(item), folderNameTransformationFunc(webhookEvent)));
                 req.Dispose();
